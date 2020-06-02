e2cb2a273d96538ba20927b9883981de76589e0e
loldabigboi <loldabigboi@gmail.com>
2020-06-02 18:29:52 +1200

    Added group view popup

---
+++ script.js
@@ -0,0 +1,12 @@
+const group_popup = document.getElementById("group-popup");
+const page_container = document.getElementById("page-container");
+
+function closeGroupPopup() {
+    group_popup.style.display = "none";
+    page_container.style.filter = "none";
+}
+
+function openGroupPopup() {
+    group_popup.style.display = "block";
+    page_container.style.filter = "blur(2px)";
+}
