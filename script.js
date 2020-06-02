4400fdba3ea709b433411c40146ee494522770fd
loldabigboi <loldabigboi@gmail.com>
2020-06-02 18:31:27 +1200

    Made back button also close popup

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ function closeGroupPopup() {
 
 function openGroupPopup() {
     group_popup.style.display = "block";
-    page_container.style.filter = "blur(2px)";
+    page_container.style.filter = "blur(3px)";
 }
