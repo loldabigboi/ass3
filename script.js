e92fdda162cc9a44029fe7244bf7db1a7a00a739
d2ci8xc5 <d2ci8xc5@tuta.io>
2020-06-04 20:16:04 +1200

    SHIT LOL

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const files_list = document.getElementById('files-list')
 const size_div = document.getElementById('file-size-container')
 const time_div = document.getElementById('file-time-container')
 const banner = document.getElementById("assignment-details")
+const container = document.getElementById("files-container")
 
 const id_dict = {
     "group-popup": document.getElementById("group-popup"),
@@ -33,12 +34,12 @@ let resizeListener = (event) => {
 window.addEventListener("resize", resizeListener);
 window.addEventListener("load", resizeListener);  // to account for when window is small when website first opened
 
-const toggleSidebar = function() {
+const toggleSidebar = function () {
     sidebar.classList.toggle("sidebar-hidden");
     sidebar_toggle.classList.toggle("rotated");
 }
 
-const resetSidebar = function() {
+const resetSidebar = function () {
     sidebar.classList.remove("sidebar-slideout");
     sidebar.classList.remove("sidebar-hidden");
     sidebar_toggle.classList.remove("rotated");
@@ -49,7 +50,7 @@ const file_items = document.getElementsByClassName("file-item");
 const file_name_items = document.getElementsByClassName("file-name-item");
 const file_containers = document.getElementsByClassName("file-container");
 
-const fileListEventWrapper = function(event, className, add) {
+const fileListEventWrapper = function (event, className, add) {
     const source = event.target || event.srcElement;
     const index = Array.from(source.parentNode.children).indexOf(source)
     for (let i_ = 0; i_ < file_containers.length; i_++) {
@@ -57,7 +58,7 @@ const fileListEventWrapper = function(event, className, add) {
             file_containers[i_].children[index].classList.add(className);
         } else {
             file_containers[i_].children[index].classList.remove(className);
-        } 
+        }
     }
 }
 
@@ -71,7 +72,7 @@ function openPopup(id) {
     page_container.addEventListener("mousedown", closePopup);
 }
 
-let closePopup = function() {
+let closePopup = function () {
     id_dict[currPopupId].style.display = "none";
     page_container.style.filter = "none";
     page_cover.style.display = "none";
@@ -79,13 +80,13 @@ let closePopup = function() {
 }
 
 function searchFilter() {
-	for(i = 0; i < files_list.children.length; ++i) {
-		if(files_list.children[i].children[0].innerText.toLowerCase().includes(search_bar.value.toLowerCase())) {
-			files_list.children[i].style.display = "flex"
-		} else {
-			files_list.children[i].style.display = "none"
-		}
-	}
+    for (i = 0; i < files_list.children.length; ++i) {
+        if (files_list.children[i].children[0].innerText.toLowerCase().includes(search_bar.value.toLowerCase())) {
+            files_list.children[i].style.display = "flex"
+        } else {
+            files_list.children[i].style.display = "none"
+        }
+    }
 }
 
 function selectAssignment(element) {
@@ -98,7 +99,7 @@ function selectAssignment(element) {
 }
 
 function populateFileList(element) {
-    if(element.classList.contains("first-assignment")) {
+    if (element.classList.contains("first-assignment")) {
         files_list.children[1].style.display = "flex"
         files_list.children[3].style.display = "flex"
         files_list.children[4].style.display = "flex"
@@ -151,11 +152,11 @@ function sortFiles(type) {
 
     let sortFunc;
     if (type === "name") {
-        sortFunc = function(e1, e2) {
+        sortFunc = function (e1, e2) {
             return Number(e1.children[0].innerText) - Number(e2.children[0].innerText);
         }
     } else {
-        sortFunc = function(e1, e2) {
+        sortFunc = function (e1, e2) {
             let fileSizeText1 = e1.children[1].innerText;
             let fileSizeText2 = e1.children[1].innerText;
 
@@ -170,7 +171,7 @@ function sortFiles(type) {
 
                             numBytes = Number.parseInt(numBytes);
                             let byteType = char;
-                            if (char = fileSizeText.charAt(i+1)) {
+                            if (char = fileSizeText.charAt(i + 1)) {
                                 byteType += char;
                             }
 
@@ -194,7 +195,7 @@ function sortFiles(type) {
     let elems = document.getElementById("file-list").children;
 
     let array = [];
-    for (let i = 0; i < elems.length; i++) { 
+    for (let i = 0; i < elems.length; i++) {
         array[i] = elems[i].cloneNode(true);
     }
 
@@ -205,7 +206,7 @@ function sortFiles(type) {
     }
     [type] = !reverseDict[type];
 
-    for (let i = 0; i < array.length; i++) { 
+    for (let i = 0; i < array.length; i++) {
         document.getElementById("file-list").replaceChild(array[i], elems[i]);
     }
 
@@ -215,4 +216,23 @@ function sortFiles(type) {
 function deleteAssignment() {
     document.getElementsByClassName("selected-assignment")[0].style.display = "none"
     document.getElementsByClassName("assignment-card")[1].click()
+}
+
+function fileUpload() {
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.click();
+    input.onchange = e => {
+        var file = e.target.files[0];
+
+        input.click();
+        console.log(file.name)
+        console.log(file.size)
+        console.log(file.type)
+        //create a div that matches
+        var div = document.createElement("div");
+        `This is  times easier!`
+        div.innerHTML = `<div class=\"file-item\"> <div class=\"file-item-section file-name-item file-name-column\"> <label> <input class=\"file-checkbox\" type=\"checkbox\"> </label> <img src=\"file-earmark-text.svg\"> <span>${file.name}</span> </div> <div class=\"file-item-section file-size-item file-size-column\"> <span>42B</span> </div> <div class=\"file-item-section file-time-item file-time-column\"> <span>11:00AM, 7th June</span> </div> </div>`
+        container.appendChild(div);
+    }
 }
